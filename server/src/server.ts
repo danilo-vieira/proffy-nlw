2b584f3064308b6c598795b8b5b885a45d4ade61
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-08-04 07:41:59 -0300

    feat: add classes and connections routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import './database/connection';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.post('/', () => {});
+app.use(routes);
 
 app.listen(3333);
