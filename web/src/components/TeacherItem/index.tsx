b89c0c8317701f0ddaeb7237212d0fc53cbf7130
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-08-06 15:13:55 -0300

    feat: connect with API

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,58 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api';
+
 import'./styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  user_id: number;
+  avatar: string;
+  bio: string;
+  cost: string;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+} 
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.user_id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/20424197?s=460&u=58e091130b59b3a0f315c327c9006e698f18a29f&v=4" alt="Danilo Vieira"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Danilo Vieira</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt 
-        <br/> <br/>
-        repudiandae, accusantium architecto, voluptate corporis obcaecati 
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button" >
+        <a 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`} 
+        >
           <img src={whatsappIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
